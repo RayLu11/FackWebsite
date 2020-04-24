84c61b4cf78fb1ee5bdada2d55608eee805e212c
RayLu11 <38595010+RayLu11@users.noreply.github.com>
2020-04-23 22:49:47 -0500

    Update page1.js

--- page1.js
+++ page1.js
@@ -1,29 +1,41 @@
 
 
+//localStorage.setItem("1",randomNumber)
 function acceptTerm()
 {
         document.querySelector('.bg-modal').style.display = "none";
+        sessionStorage.setItem("randonNumbers",1);
 }
 function declineTerm()
 {
         alert("You need accept term to continue visit website!");
 }
-
 function randomlize()
 {
-        randomNumber = Math.floor(Math.random() * 3)+1;
-        console.log(randomNumber)
-        if(randomNumber == 1)
+        if(typeof(Storage) !== "undefined") {
+                if (sessionStorage.clickcount) {
+                  sessionStorage.clickcount = Number(sessionStorage.clickcount)+1;
+                } else {
+                  sessionStorage.clickcount = 1;
+                }
+        }
+        //sessionStorage.setItem("randonNumbers","1");
+       // number = sessionStorage.getItem("randomNumbers")
+
+        if( sessionStorage.clickcount == 1)
         {
                 document.getElementById("textbox1").style.display = "flex";
         }
-        else if(randomNumber == 2)
+        else if(sessionStorage.clickcount == 2)
         {
                 document.getElementById("textbox2").style.display = "flex";
         }
-        else if(randomNumber == 3)
+        else if(sessionStorage.clickcount == 3)
         {
                 document.getElementById("textbox3").style.display = "flex";
         }
+       
+      
+      
         
 }
