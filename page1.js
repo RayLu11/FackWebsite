e1ae78794bd723835917eb827a29b6d3b84dd0be
RayLu11 <38595010+RayLu11@users.noreply.github.com>
2020-04-23 19:04:06 -0500

    Update page1.js

--- page1.js
+++ page1.js
@@ -9,3 +9,21 @@ function declineTerm()
         alert("You need accept term to continue visit website!");
 }
 
+function randomlize()
+{
+        randomNumber = Math.floor(Math.random() * 3)+1;
+        console.log(randomNumber)
+        if(randomNumber == 1)
+        {
+                document.getElementById("textbox1").style.display = "flex";
+        }
+        else if(randomNumber == 2)
+        {
+                document.getElementById("textbox2").style.display = "flex";
+        }
+        else if(randomNumber == 3)
+        {
+                document.getElementById("textbox3").style.display = "flex";
+        }
+        
+}
